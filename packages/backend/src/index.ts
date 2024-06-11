b320437bb1938f5acd2e40ca7b0faafa34afe112
Daniel Osarokutamwen <daniel@rarefyconsulting.com>
2024-06-11 10:39:23 +0100

    working prod

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -19,6 +19,7 @@ backend.add(import('@backstage/plugin-techdocs-backend/alpha'));
 backend.add(import('@backstage/plugin-auth-backend'));
 // See https://backstage.io/docs/backend-system/building-backends/migrating#the-auth-plugin
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 
 // catalog plugin
@@ -26,6 +27,7 @@ backend.add(import('@backstage/plugin-catalog-backend/alpha'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 // permission plugin
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
